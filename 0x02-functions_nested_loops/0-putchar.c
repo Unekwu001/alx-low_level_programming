d4f29a3ec5056e61666f9e47b48fa1e81d8e7e1e
Unekwu001 <unekwutheophilus@gmail.com>
2022-07-06 05:35:45 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,15 +10,15 @@
 /**
  * main - Prints "_putchar" followed by a new line.
  *
- * Return: Always (0) Success. 
+ * Return: Always (0) Success.
  */
 int main(void)
 {
-	char sapa[9] = "_putchar" ;
-	int v ;
+	char sapa[9] = "_putchar";
+	int v;
 
-	for ( v = 0; v <= 7; v++ )
+	for (v = 0; v <= 7; v++)
 		putchar(sapa[v]);
 	putchar('\n');
 	return (0);
-}	
+}
