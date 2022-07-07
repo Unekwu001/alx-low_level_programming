86ffc70da4f96a3a810af3680a5e921d1747f16d
Unekwu001 <unekwutheophilus@gmail.com>
2022-07-06 17:34:29 -0700

    Write a function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,11 @@
 /**
+ * File - 3-islower.c
+ *_islower: This function flags an alphabet that is not a lowercase alphabet
  * Author; Unekwu Shaibu
  *
- * Return: Always (1) Success
- * and (0) Always failure
- */
-#include "main.h"
-/**
- * _islower - redflags any character
- * which is not a lowercase alphabet followed by a new line
+ * Return: Always (1) Success and (0) Always failure
+ *
+ *
  * @c: single alphabeth input
  */
 int _islower(int c)
