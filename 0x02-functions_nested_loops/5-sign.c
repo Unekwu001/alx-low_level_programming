9bcb5e436081f740290cdc636c3704cbdbf80239
Unekwu001 <unekwutheophilus@gmail.com>
2022-07-06 18:45:48 -0700

    Write a function that prints the sign of a number.Prototype: int print_sign(int n)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+/**
+ * print_sign - Write a function that checks for alphabetic character
+ * File - 5-sign.c
+ *
+ * Author; Unekwu Shaibu
+ *
+ * Return: pls go through the function. you will understand
+ *
+ *
+ * @n: single alphabeth input
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (0);
+	}
+}
+
