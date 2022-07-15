c98c5270a4ce1c4505ea8ab4bb9ccd9cd9971eb1
Unekwu001 <unekwutheophilus@gmail.com>
2022-07-15 14:06:20 -0700

    Write a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - main function
+ * File - 6-abs.c
+ *
+ * Author; Unekwu Shaibu
+ *
+ * Return: returns n* -one.
+ *
+ * @n: integer to make absolute.
+*/
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
+
