664dd8c31d51b8f0ef165b11947efd3ccdc04e74
Unekwu001 <unekwutheophilus@gmail.com>
2022-07-18 05:49:27 -0700

    Write a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define NULL 0
 /**
  * _strchr - locate 1st occurence of char in string and returns pointer there
  * @s: string search.
@@ -16,5 +15,5 @@ char *_strchr(char *s, char c)
 	if (s[i] == c)
 		return (&s[i]);
 	else
-		return (NULL);
+		return (0);
 }
