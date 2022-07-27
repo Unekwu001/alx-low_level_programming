0463aae3b752c7649130a2d90ebbbe09030b1d54
Unekwu001 <unekwutheophilus@gmail.com>
2022-07-27 01:09:34 -0700

    Write a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the nuber of arguments.
+ * @argc: count of arguments.
+ * @argv: array of pointers to cli.
+ * Return: 0.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	*argv = 8argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
