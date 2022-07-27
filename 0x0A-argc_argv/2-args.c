a593ea93cc9bc449dd265524d9ff99bb2f1b70a0
Unekwu001 <unekwutheophilus@gmail.com>
2022-07-27 02:11:56 -0700

    Write a program that prints all arguments it receives.

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,16 +7,12 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 
 	int i;
 
-	i = 0;
-	while (i < argc)
-	{
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-		i++;
-	}
-	        return (0);
+	return (0);
 }
