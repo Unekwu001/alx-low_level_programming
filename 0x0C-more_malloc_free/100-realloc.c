26dd89adc2ec8a53853ee09c3c5f77ec55ad7f8f
Unekwu001 <unekwutheophilus@gmail.com>
2022-07-29 15:21:43 -0700

    Write a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,7 +36,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (nptr == NULL)
 		return (NULL);
-	for (i = 0: i < old_size && i < new_size; i++)
+	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		nptr[i] = ((char *) ptr)[i];
 	}
