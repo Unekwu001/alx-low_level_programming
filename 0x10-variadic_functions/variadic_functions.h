30d1b984c8b273f270fdda058193b6d25dfdafbb
Unekwu001 <unekwutheophilus@gmail.com>
2022-08-05 12:53:08 -0700

    Write a function that returns the sum of all its parameters.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,18 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-/**
- * struct print_all - prints anything
- * @all: arguments
- * @func: pointer to the functions that prints all
- */
-typedef struct print_all
-{
-	char all;
-	void (*func)(va_list arguments);
-}simbol_t;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
+
 #endif
