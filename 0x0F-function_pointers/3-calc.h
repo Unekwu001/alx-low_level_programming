8df5191213405397ad89276761b74ea5ece1fa59
Unekwu001 <unekwutheophilus@gmail.com>
2022-08-06 03:41:20 -0700

    Write a program that performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -21,4 +21,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_mod(int a, int b);
 int op_div(int a, int b);
-int (*get_op_func(char *s))(int, int);#endif
+int (*get_op_func(char *s))(int, int);
+
+#endif
